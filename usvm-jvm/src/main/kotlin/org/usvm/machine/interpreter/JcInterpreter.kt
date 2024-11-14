a8caaac17a620d3ce79def0566e198bba1cddfdb
Arthur Alekseev <arthur100500@gmail.com>
2024-11-15 00:28:07 +0300

    [wip] Add symbolic headers
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm/src/main/kotlin/org/usvm/machine/interpreter/JcInterpreter.kt
+++ usvm-jvm/src/main/kotlin/org/usvm/machine/interpreter/JcInterpreter.kt
@@ -175,6 +175,8 @@ class JcInterpreter(
             return scope.stepResult()
         }
 
+        logger.info(stmt.method.enclosingClass.name + "#" + stmt.method.name + " " + stmt.toString())
+
         when (stmt) {
             is JcMethodCallBaseInst -> visitMethodCall(scope, stmt)
             is JcAssignInst -> visitAssignInst(scope, stmt)
