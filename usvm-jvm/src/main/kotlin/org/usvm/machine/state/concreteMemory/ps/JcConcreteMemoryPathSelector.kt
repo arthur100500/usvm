3aa6ec8549d260c037607d99f3c69678356e7e2b
Arthur Alekseev <arthur100500@gmail.com>
2024-12-13 03:19:15 +0300

    [wip] Adding support for symbolic header/parameter/etc maps
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm/src/main/kotlin/org/usvm/machine/state/concreteMemory/ps/JcConcreteMemoryPathSelector.kt
+++ usvm-jvm/src/main/kotlin/org/usvm/machine/state/concreteMemory/ps/JcConcreteMemoryPathSelector.kt
@@ -1,8 +1,14 @@
 package org.usvm.machine.state.concreteMemory.ps
 
+import org.jacodb.api.jvm.cfg.JcExpr
+import org.usvm.UConcreteHeapRef
+import org.usvm.UHeapRef
 import org.usvm.UPathSelector
+import org.usvm.api.util.JcTestStateResolver
+import org.usvm.logger
 import org.usvm.machine.state.JcState
 import org.usvm.machine.state.concreteMemory.JcConcreteMemory
+import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 class JcConcreteMemoryPathSelector(
     private val selector: UPathSelector<JcState>
@@ -33,10 +39,25 @@ class JcConcreteMemoryPathSelector(
         selector.add(states)
     }
 
+    private fun getConcreteValue(state: JcState, expr: UConcreteHeapRef) : Any? {
+        if (expr.address == 0) return "failed to retrieve"
+        return (state.memory as JcConcreteMemory).tryHeapRefToObject(expr)
+    }
+
+    private fun printSpringTestSummary(state: JcState) {
+
+        val userDefinedValues = state.userDefinedValues
+        userDefinedValues.forEach {
+            logger.info(it.key + ": " + getConcreteValue(state, state.models[0].eval(it.value) as UConcreteHeapRef).toString())
+        }
+    }
+
+
     override fun remove(state: JcState) {
         // TODO: care about Engine.assume -- it's fork, but else state of assume is useless #CM
         check(fixedState == state)
         fixedState = null
+        printSpringTestSummary(state)
         selector.remove(state)
         (state.memory as JcConcreteMemory).kill()
         println("removed state: ${state.id}")
