0deb49a1edafc8551aabafcb790477a4182ea658
Arthur Alekseev <arthur100500@gmail.com>
2025-04-04 13:36:03 +0300

    [fix] Used array lists instead of kotlin list
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm-spring/src/main/kotlin/machine/JcSpringMethodApproximationResolver.kt
+++ usvm-jvm-spring/src/main/kotlin/machine/JcSpringMethodApproximationResolver.kt
@@ -587,14 +587,14 @@ class JcSpringMethodApproximationResolver (
         }.toList()
     }
 
-    private fun allControllerPaths(stateToFill: JcSpringState): List<List<Any>> {
+    private fun allControllerPaths(stateToFill: JcSpringState): ArrayList<ArrayList<Any>> {
         val handlerData = getHandlerData()
         stateToFill.handlerData = handlerData
 
         return handlerData
             .filterNot {shouldSkipPath(it.pathTemplate, it.handler.name, it.controller.name)}
-            .map { listOf(it.controller.name, it.handler.name, it.pathTemplate, it.uriVariablesCount, it.allowedMethods.first()) }
-            .toList()
+            .map { arrayListOf<Any>(it.controller.name, it.handler.name, it.pathTemplate, it.uriVariablesCount, it.allowedMethods.first()) }
+            .let { ArrayList(it) }
     }
 
 
