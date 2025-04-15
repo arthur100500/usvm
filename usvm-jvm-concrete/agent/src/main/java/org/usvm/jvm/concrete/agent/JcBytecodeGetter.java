f1de9a2ebd4cc9942b061428a6b11f9f8105510d
MchKosticyn <mishakosticyn@yandex.ru>
2025-04-15 16:38:44 +0300

    wip

--- usvm-jvm-concrete/agent/src/main/java/org/usvm/jvm/concrete/agent/JcBytecodeGetter.java
+++ usvm-jvm-concrete/agent/src/main/java/org/usvm/jvm/concrete/agent/JcBytecodeGetter.java
@@ -3,21 +3,24 @@ package org.usvm.jvm.concrete.agent;
 import org.usvm.jvm.concrete.JcConcreteClassLoader;
 
 import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
 public class JcBytecodeGetter implements ClassFileTransformer {
-    @Override
-    public byte[] transform(ClassLoader loader,
-                            String className,
-                            Class<?> classBeingRedefined,
-                            ProtectionDomain protectionDomain,
-                            byte[] classfileBuffer) throws IllegalClassFormatException {
-        if (className == null || !(loader instanceof JcConcreteClassLoader cl)) {
-            return classfileBuffer;
-        }
 
-        cl.addTypeBytes(className, classfileBuffer);
+    @SuppressWarnings("PatternVariableCanBeUsed")
+    @Override
+    public byte[] transform(
+        ClassLoader loader,
+        String className,
+        Class<?> classBeingRedefined,
+        ProtectionDomain protectionDomain,
+        byte[] classfileBuffer
+    ) {
+        if (className == null || !(loader instanceof JcConcreteClassLoader))
+            return classfileBuffer;
+
+        JcConcreteClassLoader classLoader = (JcConcreteClassLoader) loader;
+        classLoader.addTypeBytes(className, classfileBuffer);
 
         return classfileBuffer;
     }
