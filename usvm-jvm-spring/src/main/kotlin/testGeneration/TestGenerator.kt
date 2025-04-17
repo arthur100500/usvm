ef2fbd19a4d04d9bb6087b9ffa883013cff96bb4
Arthur Alekseev <arthur100500@gmail.com>
2025-04-18 02:37:36 +0300

    [debug] Tweak test gen (2)
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm-spring/src/main/kotlin/testGeneration/TestGenerator.kt
+++ usvm-jvm-spring/src/main/kotlin/testGeneration/TestGenerator.kt
@@ -45,6 +45,7 @@ fun JcSpringState.canGenerateTest(): Boolean {
 }
 
 data class SpringTestInfo(
+    val stateId: UInt,
     val method: JcMethod,
     val isExceptional: Boolean,
     val test: UTest,
