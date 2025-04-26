6515d42b872c1bcdc14858695e198f2fc311e57f
Arthur Alekseev <arthur100500@gmail.com>
2025-04-26 23:39:18 +0300

    [debug] Step, paths, test rendering
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm-spring-runner/src/main/kotlin/WebBench.kt
+++ usvm-jvm-spring-runner/src/main/kotlin/WebBench.kt
@@ -71,6 +71,7 @@ import java.nio.file.Path
 import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.Path
 import kotlin.io.path.PathWalkOption
+import kotlin.io.path.createFile
 import kotlin.io.path.div
 import kotlin.io.path.exists
 import kotlin.io.path.extension
