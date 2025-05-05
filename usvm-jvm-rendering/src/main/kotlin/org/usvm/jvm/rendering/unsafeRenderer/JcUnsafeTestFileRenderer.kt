a783ab4d7c3ed39e397d8892a69e39ed4ac7c971
Arthur Alekseev <arthur100500@gmail.com>
2025-05-05 16:48:16 +0300

    [debug]
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm-rendering/src/main/kotlin/org/usvm/jvm/rendering/unsafeRenderer/JcUnsafeTestFileRenderer.kt
+++ usvm-jvm-rendering/src/main/kotlin/org/usvm/jvm/rendering/unsafeRenderer/JcUnsafeTestFileRenderer.kt
@@ -13,7 +13,7 @@ open class JcUnsafeTestFileRenderer : JcTestFileRenderer {
         cu: CompilationUnit,
         importManager: JcUnsafeImportManager,
         cp: JcClasspath,
-        inlineUsvmUtils: Boolean = true
+        inlineUsvmUtils: Boolean = false
     ) : super(cu, importManager, cp) {
         this.shouldInlineUsvmUtils = inlineUsvmUtils
     }
@@ -22,7 +22,7 @@ open class JcUnsafeTestFileRenderer : JcTestFileRenderer {
         packageName: String?,
         importManager: JcUnsafeImportManager,
         cp: JcClasspath,
-        inlineUsvmUtils: Boolean = true
+        inlineUsvmUtils: Boolean = false
     ) : super(packageName, importManager, cp) {
         this.shouldInlineUsvmUtils = inlineUsvmUtils
     }
--- usvm-jvm/usvm-jvm-test-api/src/main/kotlin/org/usvm/test/api/Api.kt
+++ usvm-jvm/usvm-jvm-test-api/src/main/kotlin/org/usvm/test/api/Api.kt
@@ -64,6 +64,9 @@ class UTestMethodCall(
     }
 
     override val type: JcType? = method.enclosingClass.classpath.findTypeOrNull(method.returnType)
+    override fun toString(): String {
+        return "UTestMethodCall ${method.name}"
+    }
 }
 
 class UTestStaticMethodCall(
@@ -72,6 +75,9 @@ class UTestStaticMethodCall(
 ) : UTestCall {
     override val instance: UTestExpression? = null
     override val type: JcType? = method.enclosingClass.classpath.findTypeOrNull(method.returnType)
+    override fun toString(): String {
+        return "UTestStaticMethodCall ${method.name}"
+    }
 }
 
 class UTestConstructorCall(
