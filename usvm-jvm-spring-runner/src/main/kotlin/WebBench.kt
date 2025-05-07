1c12f94bb66055b71d572fcf5b9918bdffe54e70
Arthur Alekseev <arthur100500@gmail.com>
2025-05-07 23:42:55 +0300

    [debug] Uncommented some stuff for debug
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm-spring-runner/src/main/kotlin/WebBench.kt
+++ usvm-jvm-spring-runner/src/main/kotlin/WebBench.kt
@@ -114,7 +114,7 @@ private fun loadJHipsterBench(): BenchCp {
 
 fun main() {
     val benchCp = logTime("Init jacodb") {
-        loadWebPetClinicBench()
+        loadSynthBench()
     }
 
     logTime("Analysis ALL") {
