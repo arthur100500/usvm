b8f1d984e2eb97027e9111ed1d268f7c756ce417
Arthur Alekseev <arthur100500@gmail.com>
2025-05-14 16:16:28 +0300

    debug
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm-spring-runner/src/main/kotlin/WebBench.kt
+++ usvm-jvm-spring-runner/src/main/kotlin/WebBench.kt
@@ -444,7 +444,7 @@ private fun analyzeBench(benchmark: BenchCp) {
         pathSelectionStrategies = listOf(PathSelectionStrategy.BFS),
         coverageZone = CoverageZone.METHOD,
         exceptionsPropagation = false,
-        timeout = 2.minutes,
+        timeout = 5.minutes,
         solverType = SolverType.YICES,
         loopIterationLimit = 2,
         solverTimeout = Duration.INFINITE, // we do not need the timeout for a solver in tests
