ae7d12506b5d9929b1526740a52b1a4cc5afa6a7
Arthur Alekseev <arthur100500@gmail.com>
2025-05-15 21:26:21 +0300

    [fix] Fixed primitive pinned value not unboxing from argument + matrix support
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm-spring/src/main/kotlin/machine/JcSpringMethodApproximationResolver.kt
+++ usvm-jvm-spring/src/main/kotlin/machine/JcSpringMethodApproximationResolver.kt
@@ -582,6 +582,7 @@ class JcSpringMethodApproximationResolver (
 
     @Suppress("UNUSED_PARAMETER")
     private fun shouldAnalyzePath(path: String, handlerName: String, controllerTypeName: String): Boolean {
+        // skibidi
         return true
     }
 
