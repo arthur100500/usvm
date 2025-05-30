afba1825f869fa49d4b9602c6034ffb2cca3ea31
Arthur Alekseev <arthur100500@gmail.com>
2025-05-30 14:12:00 +0300

    [debug] Change dirs for laptop + add env bench
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm-spring-runner/src/main/kotlin/WebBench.kt
+++ usvm-jvm-spring-runner/src/main/kotlin/WebBench.kt
@@ -84,7 +84,7 @@ import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.nanoseconds
 
 private fun loadWebPetClinicBench(): BenchCp {
-    val petClinicDir = Path("C:\\Users\\arthur\\Documents\\spring-petclinic\\build\\libs\\BOOT-INF")
+    val petClinicDir = Path("C:\\Users\\arthu\\Documents\\spring-petclinic\\build\\libs\\BOOT-INF")
     return loadWebAppBenchCp(petClinicDir / "classes", petClinicDir / "lib")
 }
 
@@ -113,9 +113,14 @@ private fun loadJHipsterBench(): BenchCp {
     return loadWebAppBenchCp(benchDir / "classes", benchDir / "lib")
 }
 
+private fun loadBenchFromEnv(): BenchCp {
+    val benchDir = Path(System.getenv("usvm.benchmark"))
+    return loadWebAppBenchCp(benchDir / "classes", benchDir / "lib")
+}
+
 fun main() {
     val benchCp = logTime("Init jacodb") {
-        loadWebPetClinicBench()
+        loadBenchFromEnv()
     }
 
     logTime("Analysis ALL") {
