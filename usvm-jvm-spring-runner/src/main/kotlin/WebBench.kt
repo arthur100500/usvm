9866ca9d3a6427b4c4a40b3905c4045daa73ea0c
Arthur Alekseev <arthur100500@gmail.com>
2025-06-18 03:50:09 +0300

    [feat] Add new test for array copy
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm-spring-runner/src/main/kotlin/WebBench.kt
+++ usvm-jvm-spring-runner/src/main/kotlin/WebBench.kt
@@ -450,7 +450,7 @@ private fun analyzeBench(benchmark: BenchCp) {
         pathSelectionStrategies = listOf(PathSelectionStrategy.BFS),
         coverageZone = CoverageZone.METHOD,
         exceptionsPropagation = false,
-        timeout = 5.minutes,
+        timeout = 1.minutes,
         solverType = SolverType.YICES,
         loopIterationLimit = 2,
         solverTimeout = Duration.INFINITE, // we do not need the timeout for a solver in tests
