1cd6fdeb4df1977d1918b1cc76875427e9c8ef1d
Arthur Alekseev <arthur100500@gmail.com>
2025-06-30 14:57:23 +0300

    [feat] Sublist test
    Signed-off-by: Arthur Alekseev <arthur100500@gmail.com>

--- usvm-jvm/src/test/kotlin/org/usvm/samples/collections/ArrayListTest.kt
+++ usvm-jvm/src/test/kotlin/org/usvm/samples/collections/ArrayListTest.kt
@@ -2,9 +2,10 @@ package org.usvm.samples.collections
 
 import org.junit.jupiter.api.Test
 import org.usvm.samples.JavaMethodTestRunner
+import org.usvm.samples.approximations.ApproximationsTestRunner
 import org.usvm.test.util.checkers.ignoreNumberOfAnalysisResults
 
-class ArrayListCopyTest : JavaMethodTestRunner()  {
+class ArrayListCopyTest : ApproximationsTestRunner()  {
     @Test
     fun testArrayListConstructorTest() {
         checkDiscoveredPropertiesWithExceptions(
@@ -12,6 +13,17 @@ class ArrayListCopyTest : JavaMethodTestRunner()  {
             ignoreNumberOfAnalysisResults,
             invariants = arrayOf(
                 { arr, result -> result.getOrNull() != false },
+            )
+        )
+    }
+
+    @Test
+    fun testArrayListSublistIteration() {
+        checkDiscoveredPropertiesWithExceptions(
+            ArrayListSamples::arrayListSublistIteration,
+            ignoreNumberOfAnalysisResults,
+            invariants = arrayOf(
+                { arr, result -> result.getOrNull() == true },
             )
         )
     }
